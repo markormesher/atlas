69d18cd84d6ccf660442550e30eaa0a6ba6c6300
Mark Ormesher <me@markormesher.co.uk>
2018-01-27 18:43:29 +0000

    Clean up some code

--- app/app.js
+++ app/app.js
@@ -1,18 +1,17 @@
 const path = require('path');
 const express = require('express');
 const request = require('request');
-const getPlaces = require('./get-places');
+const Storage = require('node-storage');
 const configLoader = require('./helpers/config-loader');
 
 const googleApiKey = configLoader.getSecret('google.api.key');
+const placeStore = new Storage('/data/places');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/places', (req, res) => {
-	res.json(getPlaces());
-});
+app.get('/places', (req, res) => res.json(placeStore.get('places') || []));
 
 app.get('/google-maps-api', (req, res) => {
 	request('https://maps.googleapis.com/maps/api/js?key=' + googleApiKey + '&callback=initMap', (err, full, body) => {
--- app/get-places.js
+++
@@ -1,8 +0,0 @@
-const Storage = require("node-storage");
-
-const getPlaces = () => {
-	const store = new Storage("/data/places");
-	return store.get("places") || [];
-};
-
-module.exports = getPlaces;
